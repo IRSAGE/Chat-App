e1fc85293965ee2c5f79afb417efccdf8a80c448
IRSAGE <sibo.egide98@gmail.com>
2021-10-14 11:17:41 +0200

    Setting Up the chatRommSCreen

---
+++ screens/ChatRoomSCreen.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+
+const ChatRoomSCreen = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default ChatRoomSCreen;
+
+const styles = StyleSheet.create({});
